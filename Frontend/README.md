a9e01e8e6fdf28d62d0692a4a1cc724c438a72ef
NBecic <nikola.becic.4360@metropolitan.ac.rs>
2024-01-21 15:48:25 +0100

    Fix

--- Frontend/README.md
+++ Frontend/README.md
@@ -1,4 +1,4 @@
-# Se325Frontend
+# Se401Frontend
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.2.8.
 
--- Frontend/karma.conf.js
+++ Frontend/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/se325_frontend'),
+      dir: require('path').join(__dirname, './coverage/se401_frontend'),
       subdir: '.',
       reporters: [
         { type: 'html' },
